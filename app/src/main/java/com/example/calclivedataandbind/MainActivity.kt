e5200c01ed71ff6d17a191cacb7b7afc22f3ffd2
“Yasunori <yasunori-matsuda@cookpad.jp>
2023-01-03 00:55:53 +0900

    Refactor view model

--- app/src/main/java/com/example/calclivedataandbind/MainActivity.kt
+++ app/src/main/java/com/example/calclivedataandbind/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.lifecycleOwner = this
         binding.viewModel = viewModel
     }
 }
--- app/src/main/java/com/example/calclivedataandbind/ViewModel.kt
+++ app/src/main/java/com/example/calclivedataandbind/ViewModel.kt
@@ -1,15 +1,20 @@
 package com.example.calclivedataandbind
 
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ViewModel : ViewModel() {
+    val first = MutableLiveData("")
+    val second = MutableLiveData("")
+    val result = MediatorLiveData<String>().apply {
+        addSource(first) { value = calc() }
+        addSource(second) { value = calc() }
+    }
 
-    val first = MutableLiveData<String>()
-    val second = MutableLiveData<String>()
-    val result = INIT_VAL
-
-    companion object {
-        const val INIT_VAL = "0"
+    private fun calc(): String {
+        val firstValue = if (first.value.isNullOrBlank()) 0 else first.value!!.toInt()
+        val secondValue = if (second.value.isNullOrBlank()) 0 else second.value!!.toInt()
+        return (firstValue + secondValue).toString()
     }
 }
